f3858a64b50cbe9b7acaff6e20b3c26aa8c07a40
david-parkk <ji0513ji@naver.com>
2023-06-05 01:35:18 +0900

    fixed ranking font size & feat 성인 청소년구분

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -61,24 +61,26 @@
         </li>
       </ul>
     </div>
-    <div class="h2">
-       위험성 첼린지 
+    <div v-if="adult">
+      <div class="h2">
+        위험성 첼린지 
+      </div>
+      <div id="amusement_challenge">
+        <ul>
+          <li v-for="(category, key,index) in danger_category" :key="key" class="challenge_category"   @click="send_danger_router(key)">
+            <div :class="['challenge_boxin', `dance-${index}`]" :style="{ backgroundImage: `url(/img/dance${index}.png)` }">
+            <div class="category_name">
+              {{ key }}
+            </div>
+            <ul class="challenge_value">
+              <li v-for="value in category" :key="value">
+                {{ value }}
+              </li>
+            </ul>
+            </div>
+          </li>
+        </ul>
     </div>
-    <div id="amusement_challenge">
-      <ul>
-        <li v-for="(category, key,index) in danger_category" :key="key" class="challenge_category"   @click="send_danger_router(key)">
-          <div :class="['challenge_boxin', `dance-${index}`]" :style="{ backgroundImage: `url(/img/dance${index}.png)` }">
-          <div class="category_name">
-            {{ key }}
-          </div>
-          <ul class="challenge_value">
-            <li v-for="value in category" :key="value">
-              {{ value }}
-            </li>
-          </ul>
-          </div>
-        </li>
-      </ul>
     </div>
     <div class="h2">
        기타 첼린지 
@@ -105,13 +107,15 @@
 
 <script>
 import {view_home,view_home_detail} from '../api/homeapi.js'
+import {check_user}from '../api/login.js';
+import Store from '../api/store.js';
 // @ is an alias to /src
 export default{
   data(){
     return{
-      
-      dance_category: {"k-pop":[],"j-pop":["adsf"],"pop":["Asdfaf"]},
-      morality_category:{"donation":["asdfadf"],"covid":["asdfafd"]},
+      adult:false,
+      dance_category: {"K-POP":[],"J-POP":[],"POP":[]},
+      morality_category:{"donation":[],"covid":[]},
       fun_category:{"pet":["유기견보호","ㅁㄴㅇㄹ","ㅁㄴㅇㄹ","ㅁㄴㅇㄹ","ㅁㄴㅇㄹ","ㅁㄴㅇㄹ"],"finger":["asdfa"]},
       danger_category:{"danger":["asdfasdf"]},
       other_category:{"기타":["adsafsd"]}
@@ -119,31 +123,49 @@ export default{
   },
   async mounted(){
     try{
+        Store.commit('checktoken')  
+        const check=await check_user(Store.getters.get_token)
+        console.log("check",check)
+        if(check.age>=20){
+          this.adult=true;
+        }
+        console.log("check",check)
         view_home().then((array)=>{
-        for(var i=0;(i<array.length&&i<3);i++){
+        for(var i=0;(i<array.length);i++){
           if(array[i]["rate"]<7){
             if (Object.prototype.hasOwnProperty.call(this.dance_category, array[i]["smallcategory"])) {
-              this.dance_category["위험성"].push(array[i]['subject']);
+              if(this.dance_category["danger"].length<3){
+                this.dance_category["danger"].push(array[i]['subject']);
+              }
             }
           }
           if(array[i]["Bigcategory"].includes('댄스')){
             if (Object.prototype.hasOwnProperty.call(this.dance_category, array[i]["smallcategory"])) {
-              this.dance_category[array[i]["smallcategory"]].push(array[i]['subject']);
+              if(this.dance_category[array[i]["smallcategory"]].length<3){
+                this.dance_category[array[i]["smallcategory"]].push(array[i]['subject']);
+              }
+              
             }
           }
           else if(array[i]["Bigcategory"].includes('공익성')){
             if (Object.prototype.hasOwnProperty.call(this.morality_category, array[i]["smallcategory"])) {
-              this.morality_category[array[i]["smallcategory"]].push(array[i]['subject']);
+              if(this.dance_category[array[i]["smallcategory"]].length<3){
+                this.morality_category[array[i]["smallcategory"]].push(array[i]['subject']);
+              }
             }
           }
           else if(array[i]["Bigcategory"].includes('유희')){
             if (Object.prototype.hasOwnProperty.call(this.fun_category, array[i]["smallcategory"])) {
-              this.fun_category[array[i]["smallcategory"]].push(array[i]['subject']);
+              if(this.dance_category[array[i]["smallcategory"]].length<3){
+                this.fun_category[array[i]["smallcategory"]].push(array[i]['subject']);
+              }
             }
           }
 
           else{
-            this.other_category["other"].push(array[i]["smallcategory"]);
+            if(this.dance_category["other"].length<3){
+              this.other_category["other"].push(array[i]["smallcategory"]);
+            }
           }
           
         }
